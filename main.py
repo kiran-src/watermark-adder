e30448e512c5d43dfecee9f3d5f4cd9850b5bd9e
Kiran <kiransettyks@gmail.com>
2022-04-05 19:54:51 +0200

    Upload button has functionality and retrieves image filepath for making changes to Filetype of image is checked and ensured is of the supported type

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from tkinter import *
+from tkinter import filedialog as fd
 from PIL import Image, ImageTk
+
+accepted_types = ['png', 'jpg', 'bmp']
 
 window = Tk()
 window.title("Watermark Adder")
@@ -12,6 +15,9 @@ heading.grid(column=0, row=0, columnspan=2)
 heading = Label(text="Example of watermark", font=("Arial", 12, "bold"), fg='black')
 heading.grid(column=0, row=2)
 
+instructions = Label(text="", font=("Arial", 10), fg='black')
+instructions.grid(column=1, row=2)
+
 canvas = Canvas(width=200, height=300, highlightthickness=0)
 image = PhotoImage(file="silhouette.png")
 image2 = image.subsample(13, 13)
@@ -20,7 +26,18 @@ canvas.grid(column=0, row=3)
 
 
 def press():
-    pass
+    image_path = fd.askopenfilename()
+    print(image_path)
+    if image_path.split('.')[1] not in accepted_types:
+        instructions.config(text="Image filetype is not supported")
+    else:
+        image = Image.open(image_path)
+        width, height = image.size
+        image.show()
+        watermark = Image.open('silhouette.png')
+        watermark.thumbnail((int(width/2), int(height/2)))
+        instructions.config(text="Image saved.")
+
 
 
 upload = Button(command=press, text="Upload")
